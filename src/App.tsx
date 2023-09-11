7b2685d4b0396b9a3cf20b6c7654f0df33fabfb3
viruz <nahuelsinka@gmail.com>
2023-09-11 01:37:55 -0300

    bug fixed

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ function App(): JSX.Element {
       <Switch>
         <Route path="/" component={Initial} />
 
-        <Router>
+        <Router base="/app">
           <Route path="/saved" component={Saved} />
           <Route path="/home" component={Home} />
           <Route path="/profile" component={Profile} />
